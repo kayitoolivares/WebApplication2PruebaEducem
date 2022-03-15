44b5761b019bb1457a58f176b98b8cc8aedb1efd
Leo <kayo.olivares@gmail.com>
2022-03-15 12:44:40 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication2PruebaEducem/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication2PruebaEducem.Models;
+
+namespace WebApplication2PruebaEducem.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index(string msg="")
+        {
+            ViewBag.msg = msg;  
+            return View();
+        }
+
+
+       
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", new { msg = "Sesion cerrada" });
+        }
+
+        public ActionResult IndexDentro()
+        {
+            return RedirectToAction("Index", "Perfil");
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(string usuario, string pass)
+        {
+            if (!string.IsNullOrEmpty(usuario) && !string.IsNullOrEmpty(pass))
+            {
+                educemPruebaEntities1 db = new educemPruebaEntities1();
+                var usr= db.usuario.Select(c=> new  { c.id, c.id_personal, c.tipo, c.usuario1, c.pass }).FirstOrDefault(e=> e.usuario1 == usuario && e.pass  ==pass);
+
+                if (usr != null)
+                {
+
+                    FormsAuthentication.SetAuthCookie(usr.usuario1, true);
+                    FormsAuthentication.SetAuthCookie (usr.usuario1 + "|" + usr.tipo + "|" +usr.id, true);
+                    //System.Web.HttpContext.Current.Session["ususario"] = usr.usuario1;
+                    //System.Web.HttpContext.Current.Session["Tipo"] = usr.tipo;
+                    //System.Web.HttpContext.Current.Session["id_usuario"] = usr.id;
+                    return RedirectToAction("Index", "Perfil");
+                   
+
+                }
+                else
+                {
+                    return RedirectToAction("Index", new { msg = "usuario o password incorrecto" });
+                  
+
+
+                }
+            }
+            else{
+                return RedirectToAction("Index", new { msg = "Llenar datos" });
+                //return Index("Llenar datos");
+            }
+
+
+        }
+
+        
+       
+
+
+    }
+
+   
+
+}
---
+++ WebApplication2PruebaEducem/Controllers/PerfilController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WebApplication2PruebaEducem.Models;
+using System.Net;
+
+namespace WebApplication2PruebaEducem.Controllers
+{
+    [Authorize]
+    public class PerfilController : Controller
+    {
+        private educemPruebaEntities1 db = new educemPruebaEntities1();
+        // GET: Perfil
+
+        public ActionResult Index()
+        {
+
+             
+           
+            //var ntc = db.nota.Select(c => new { c.id, c.contenido, c.usuario.personal.nombre, c.usuario.personal.apematerno, c.fecha});
+            //ViewBag.Lista = ntc.ToList();
+
+            var nota = db.nota.Include(n => n.usuario).OrderByDescending(c=> c.fecha );
+            return View(nota.ToList());
+
+            //return View();
+        }
+
+        public  ActionResult Comentar(int? id)
+        {
+
+            if (id==null)
+            {
+                try
+                {
+                    id = Convert.ToInt32(TempData["Str"]);
+
+                }
+                catch (Exception)
+                {
+
+                    
+                }
+
+            }
+
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            nota nota = db.nota.Find(id);
+            if (nota == null)
+            {
+                return HttpNotFound();
+            }
+            TempData["Str"] = id;
+            xxModel model = new xxModel();
+            //var nota1 = db.nota.Include(n => n.usuario).OrderByDescending(c => c.fecha);
+            var nota2 = db.notaComentario.Where(e => e.id_nota == id).Include(n => n.usuario).OrderByDescending(c => c.fecha);
+            model.List1 = nota;
+            model.List2 = nota2.ToList();
+            return View(model);
+            //return View(comentarios.ToList());
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Create(string contenido)
+        {
+
+            //[Bind(Include = "id,id_usuario,fecha,contenido")] ;
+            nota nota=new nota();
+            int userId = 0; 
+            if (User.Identity.IsAuthenticated) 
+            { 
+                userId = Convert.ToInt32(User.Identity.Name.Split('|')[2]);
+            }
+
+
+
+            nota.id_usuario = userId;
+            nota.fecha = DateTime.Now;
+            nota.contenido = contenido;
+
+            if (ModelState.IsValid)
+            {
+                db.nota.Add(nota);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_usuario = new SelectList(db.usuario, "id", "usuario1", nota.id_usuario);
+            return View(nota);
+        }
+
+        [HttpPost]
+        public ActionResult CreateComentario(int? idNota, string contenido )
+        {
+
+            //[Bind(Include = "id,id_usuario,fecha,contenido")] ;
+            notaComentario nota = new notaComentario();
+            int userId = 0;
+            if (User.Identity.IsAuthenticated)
+            {
+                userId = Convert.ToInt32(User.Identity.Name.Split('|')[2]);
+            }
+
+
+
+            nota.id_usuario = userId;
+            nota.fecha = DateTime.Now;
+            nota.contenido = contenido;
+            nota.id_nota = Convert.ToInt32( TempData["Str"]);
+
+            if (ModelState.IsValid)
+            {
+                db.notaComentario.Add(nota);
+                db.SaveChanges();
+                return RedirectToAction("Comentar/"+ idNota );
+            }
+
+            ViewBag.id_usuario = new SelectList(db.usuario, "id", "usuario1", nota.id_usuario);
+            return View(nota);
+        }
+    }
+
+    public class xxModel
+    {
+
+        public nota List1 { get; set; }
+
+        public List<notaComentario> List2 { get; set; }
+
+    }
+
+
+}
